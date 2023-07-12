e15498097746ed58dafa9388fedb5c1ab694d72d
Alex Bernt <alexbernt100@gmail.com>
2023-07-11 23:31:28 -0500

    import app.js

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const express = require("express");
+const volleyball = require("volleyball");
+const cors = require("cors");
+const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(cors());
+app.use(volleyball);
+
+app.use("/api", require("./api"));
+
+app.use("*", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error");
+});
+
+module.exports = app;
