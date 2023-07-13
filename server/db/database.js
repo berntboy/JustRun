b64933d7293b6e4884bcbacb247f4ba286e6e7d0
Alex Bernt <alexbernt100@gmail.com>
2023-07-12 22:02:12 -0500

    Update imports in db

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,21 @@
-const Sequelize = require("sequelize");
+// import Sequelize from "sequelize";
+
+// const db = new Sequelize(
+//   process.env.DATABASE_URL || "postgres://localhost:5432/runners",
+//   {
+//     logging: false,
+//   }
+// );
+
+// export default db;
+
+import Sequelize from "sequelize";
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || "postgres://localhost:5432/runners",
+  import.meta.env.VITE_DATABASE_URL || "postgres://localhost:5432/runners",
   {
     logging: false,
   }
 );
 
-module.exports = db;
+export default db;
